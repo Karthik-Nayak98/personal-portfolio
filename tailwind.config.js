f961842633daf117c1905a4d20ab794cba29ac48
Karthik <karunayak63@gmail.com>
2022-02-17 16:08:33 +0530

    chore: add custom colors to project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      accent: '#FA7268',
+      black: '#191923',
+      light: '#FBFEF9',
+      dark: '#084C61',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
