882c33dffa61ed433c18f6bd90e8bead3f473f9d
Karthik <karunayak63@gmail.com>
2022-02-20 22:13:41 +0530

    feat: connect react app with sanity io

--- src/client.js
+++ src/client.js
@@ -1,6 +1,14 @@
 import sanityClient from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
 
-export default sanityClient({
-  projectId: 'x913s81c',
+export const client = sanityClient({
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: 'production',
+  apiVersion: '2022-02-19',
+  useCdn: true,
+  token: process.env.REACT_APP_SANITY_TOKEN,
 });
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
