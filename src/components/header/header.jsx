5529f069af32c60e1de6ec44a728478c97ccb30a
Karthik <karunayak63@gmail.com>
2022-04-04 23:18:28 +0530

    style: add responsive css.

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Header = ({ title }) => {
   return (
-    <div className='relative w-fit px-10 pb-2 text-3xl font-bold capitalize lg:my-3 lg:mx-52 lg:px-8 lg:text-4xl'>
-      <h2 className='text-light'>{title}</h2>
-      <div className='border-accent absolute bottom-0 w-1/2 border-2'></div>
+    <div className='relative w-fit px-10 pb-2 text-2xl lg:my-3 lg:mx-52 lg:px-12 lg:text-3xl'>
+      <h2 className='text-light font-semibold uppercase'>{title}</h2>
+      <div className='border-accent absolute bottom-0 w-1/4 border-2'></div>
     </div>
   );
 };
