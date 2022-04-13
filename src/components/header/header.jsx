ae289f0dd0669c9a87b0d6d4a97a000fe1b81d9e
Karthik <karunayak63@gmail.com>
2022-04-13 23:46:55 +0530

    feat: add header component
    * full responsive
    * has dark mode and light mode toggle icon

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 
 const Header = ({ title }) => {
   return (
-    <div className='relative w-fit px-10 pb-2 text-2xl lg:my-3 lg:mx-52 lg:px-12 lg:text-3xl'>
-      <h2 className='text-light font-semibold uppercase'>{title}</h2>
-      <div className='border-accent absolute bottom-0 w-1/4 border-2'></div>
+    <div className='relative w-fit px-8 pb-2 text-2xl md:px-12 md:text-3xl lg:my-3 lg:px-16 lg:text-4xl'>
+      <h2 className='text-light font-bold capitalize'>{title}</h2>
+      <div className='border-accent absolute bottom-0 w-2/5 border-2'></div>
     </div>
   );
 };
 
 export default Header;
+
+// <div className='relative w-fit px-10 pb-2 text-2xl lg:my-3 lg:mx-40 lg:px-12 lg:text-3xl'>
+//   <h2 className='text-light font-semibold uppercase'>{title}</h2>
+//   <div className='border-accent absolute bottom-0 w-1/4 border-2'></div>
+// </div>
