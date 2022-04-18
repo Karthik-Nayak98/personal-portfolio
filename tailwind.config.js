7c20532d4d673f27a394164e3f5dedfdb5c46368
Karthik <karunayak63@gmail.com>
2022-04-18 17:03:33 +0530

    chore: add packages and custom styles.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,23 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    screens: {
+      xs: '475px',
+      ...defaultTheme.screens,
+    },
     extend: {
       colors: {
-        primary: '#DCDCDC',
-        black: '#191923',
-        light: '#FFFFF0',
-        dark: '#010127',
+        accent: '#A3A3FF',
+        dark: '#17171F',
+        light: '#EAEAEA',
       },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
       },
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 };
