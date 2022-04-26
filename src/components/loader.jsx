c63c3956bca3f7d1b7ff3be63a898b0b1c2bd36c
Karthik <karunayak63@gmail.com>
2022-04-26 17:36:54 +0530

    feat: add loader component

---
+++ src/components/loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Loader() {
+  return (
+    <div className='transfrom absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+      <div className='border-x-dark dark:border-x-light loader animate-spin-slow after:border-x-darkAccent relative box-border h-14 w-14 rounded-full border-2 border-y-transparent after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:transform after:rounded-full after:border-[22px] after:border-y-transparent'></div>
+    </div>
+  )
+}
+
+export default Loader
