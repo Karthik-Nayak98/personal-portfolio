19b348fcba9a7c2d95c2d41bbcd45e7339b162f9
Karthik <karunayak63@gmail.com>
2022-04-27 22:21:38 +0530

    chore: add firebase package & styles for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,25 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class',
   theme: {
     screens: {
       xs: '475px',
       ...defaultTheme.screens,
     },
     extend: {
+      keyframes: {
+        rotation: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
+      animation: {
+        'spin-slow': 'rotation 1s ease-in-out infinite',
+      },
       colors: {
+        darkAccent: '#818CF8',
         accent: '#A3A3FF',
         dark: '#17171F',
         light: '#EAEAEA',
@@ -20,4 +31,4 @@ module.exports = {
     },
   },
   plugins: [require('@tailwindcss/typography')],
-};
+}
