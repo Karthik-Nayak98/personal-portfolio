d1b765acfb472c493aece1619dadeae5a27cb98f
Karthik <karunayak63@gmail.com>
2022-04-28 08:09:45 +0530

    update: add firebase config

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app'
+import { getDatabase } from 'firebase/database'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+}
+
+const app = initializeApp(firebaseConfig)
+
+const db = getDatabase(app)
+
+export { db }
