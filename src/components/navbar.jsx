d88fdf3c70c06fbea647b2766e6f74eeec9efe2f
Karthik <karunayak63@gmail.com>
2022-04-28 20:59:24 +0530

    feat: add navbar component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { BiSun, BiMoon } from 'react-icons/bi'
 import useSanity from '../hooks/useSanity'
 import NavItem from './navitem'
+import { ThemeContext } from '../context/theme-context'
 
 function Navbar() {
   const [isNavbarOpen, setIsNavbarOpen] = useState(false)
-  const [isDarkModeOn, setIsDarkModeOn] = useState(true)
+
+  const { theme, setTheme } = useContext(ThemeContext)
+
+  const toggleTheme = () => {
+    if (theme === 'dark') setTheme('light')
+    else setTheme('dark')
+  }
 
   const query = `*[_type == "author"]{image{ asset->{ url, _id,}}}`
   const authorImage = useSanity(null, query)
 
   return (
-    <header className='bg-dark container sticky top-0 z-20 mx-auto flex w-full max-w-4xl items-center justify-between px-8 py-6 md:px-12 lg:px-16'>
+    <header className='dark:bg-dark container sticky top-0 z-20 mx-auto flex w-full max-w-4xl items-center justify-between bg-gray-200 px-8 py-6 md:px-12 lg:px-16'>
       <Link to='/'>
         <img
           className='h-12 w-12 rounded-full object-cover'
@@ -22,10 +29,10 @@ function Navbar() {
       </Link>
 
       <div
-        onClick={() => setIsDarkModeOn(!isDarkModeOn)}
-        className='ml-auto mr-2 cursor-pointer rounded bg-white bg-opacity-10 p-2 md:hidden'>
-        <BiMoon className={`${isDarkModeOn ? 'hidden' : 'block'}`} size={20} />
-        <BiSun className={`${!isDarkModeOn ? 'hidden' : 'block'}`} size={20} />
+        onClick={toggleTheme}
+        className='ml-auto mr-2 cursor-pointer rounded bg-gray-200 p-2 dark:bg-white dark:bg-opacity-10 md:hidden'>
+        <BiMoon className={`${theme === 'dark' ? 'hidden' : 'block'}`} size={20} />
+        <BiSun className={`${theme === 'light' ? 'hidden' : 'block'}`} size={20} />
       </div>
       <nav className=''>
         <svg
@@ -49,7 +56,7 @@ function Navbar() {
             strokeLinejoin='round'></path>
         </svg>
         <ul
-          className={`bg-dark fixed top-20 left-0 right-0 transform items-center gap-10 space-y-2 px-6 pb-4 ${
+          className={`dark:bg-dark fixed top-20 left-0 right-0 transform items-center gap-10 space-y-2 bg-gray-200 px-6 pb-4 ${
             isNavbarOpen ? 'translate-x-0' : 'translate-x-full'
           } md:relative md:top-0 md:flex md:translate-x-0 md:gap-0 md:space-y-0 md:px-0 md:pb-0`}>
           <NavItem navtext='home' route='/' />
@@ -57,10 +64,16 @@ function Navbar() {
           <NavItem navtext='work' route='/work' />
           <NavItem navtext='about' route='/about' />
           <li
-            onClick={() => setIsDarkModeOn(!isDarkModeOn)}
-            className='ml-2 hidden cursor-pointer rounded bg-white bg-opacity-10 p-2 md:block'>
-            <BiMoon className={`${isDarkModeOn ? 'hidden' : 'block'}`} size={20} />
-            <BiSun className={`${!isDarkModeOn ? 'hidden' : 'block'}`} size={20} />
+            onClick={toggleTheme}
+            className='ml-2 hidden cursor-pointer rounded-md bg-gray-500 bg-opacity-10 p-2 dark:bg-white dark:bg-opacity-10 md:block'>
+            <BiMoon
+              className={`${theme === 'dark' ? 'hidden' : 'block'}`}
+              size={20}
+            />
+            <BiSun
+              className={`${theme === 'light' ? 'hidden' : 'block'}`}
+              size={20}
+            />
           </li>
         </ul>
       </nav>
