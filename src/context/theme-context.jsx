b69ff8c8bc598be2b17f2e1a95b1a7918813a206
Karthik <karunayak63@gmail.com>
2022-04-29 10:23:11 +0530

    feat: context api to toggle dark and light mode

---
+++ src/context/theme-context.jsx
@@ -0,0 +1,46 @@
+import React, { createContext, useEffect, useState } from 'react'
+
+const getInitialTheme = (_) => {
+  if (typeof window !== 'undefined' && window.localStorage) {
+    const storedPrefs = window.localStorage.getItem('color-theme')
+    if (typeof storedPrefs === 'string') return storedPrefs
+  }
+
+  // Checking the theme in the OS.
+  const userMedia = window.matchMedia('(prefers-color-scheme:dark)')
+  if (userMedia.matches) return 'dark'
+
+  // Set the default theme as 'dark'
+  return 'dark'
+}
+
+export const ThemeContext = createContext()
+
+export const ThemeProvider = ({ initialTheme, children }) => {
+  const [theme, setTheme] = useState(getInitialTheme)
+
+  const rawSetTheme = (theme) => {
+    const root = window.document.documentElement
+
+    const isDark = theme === 'dark'
+
+    root.classList.remove(isDark ? 'light' : 'dark')
+    root.classList.add(theme)
+
+    localStorage.setItem('color-theme', theme)
+  }
+
+  if (initialTheme) rawSetTheme(initialTheme)
+
+  useEffect(() => {
+    rawSetTheme(theme)
+  }, [theme])
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+export default ThemeProvider
