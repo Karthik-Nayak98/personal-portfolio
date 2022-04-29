0fb97fbda1ae1f820caf628633a2af6a27d01513
Karthik <karunayak63@gmail.com>
2022-04-29 10:22:50 +0530

    feat: add routes to different pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import { Routes, Route } from 'react-router-dom'
+
+import Navbar from './components/navbar'
+import Home from './pages/home'
+import Blog from './pages/blog'
+import About from './pages/about'
+import Work from './pages/work'
+import Contact from './pages/contact'
+import Post from './pages/post'
+import Error from './pages/error'
+import ProjectDescription from './pages/project-description'
+
+import ThemeProvider from './context/theme-context'
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <ThemeProvider>
+      <main className='font-inter dark:bg-dark text-dark dark:text-light min-h-screen bg-gray-200 pb-12'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/blog' element={<Blog />} />
+          <Route path='/blog/:slug' element={<Post />} />
+          <Route path='/work' element={<Work />} />
+          <Route path='/work/:project' element={<ProjectDescription />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/about' element={<About />} />
+          <Route path='*' element={<Error />} />
+        </Routes>
+      </main>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -6,11 +6,9 @@ import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
