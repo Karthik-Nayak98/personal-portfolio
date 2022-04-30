c01f878fcba7efc1d2d4dfaccec40d50e26dde43
Karthik <karunayak63@gmail.com>
2022-04-30 22:26:22 +0530

    feat: add hover animation

--- src/components/skill.jsx
+++ src/components/skill.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { urlFor } from '../utils/client'
 
+import { motion } from 'framer-motion'
+
 function Skill({ image, skill }) {
   return (
-    <article className='dark:bg-light box-border flex h-[72px] w-32 flex-col items-center justify-center gap-1 rounded-md bg-gray-100 drop-shadow-xl md:w-28'>
+    <motion.article
+      whileHover={{ y: -5 }}
+      className='dark:bg-light box-border flex h-[72px] w-32 flex-col items-center justify-center gap-1 rounded-md bg-gray-100 drop-shadow-xl md:w-28'>
       <img className='h-8 w-8' src={urlFor(image)} alt={skill} />
       <span className='text-dark text-sm font-medium'>{skill}</span>
-    </article>
+    </motion.article>
   )
 }
 
--- src/components/social-icon.jsx
+++ src/components/social-icon.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { urlFor } from '../utils/client'
+import { motion } from 'framer-motion'
 
 function SocialIcon({ name, url, icon }) {
   return (
-    <span className='bg-light rounded-lg p-1 drop-shadow-xl'>
+    <motion.span
+      whileHover={{ y: -5 }}
+      className='bg-light cursor-pointer rounded-lg p-1 drop-shadow-xl'>
       <a href={url} target='_blank' rel='noreferrer'>
         <li className='p-[1px]'>
           <img src={urlFor(icon)} alt={name} className='h-6 w-6' />
         </li>
       </a>
-    </span>
+    </motion.span>
   )
 }
 
