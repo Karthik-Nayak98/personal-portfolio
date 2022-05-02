4d15898edaa7049d07df32d2697ea3b5bc297248
Karthik <karunayak63@gmail.com>
2022-05-02 22:37:28 +0530

    refactor: add lazy for performance reasons

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
+import { lazy, Suspense } from 'react'
 import { Routes, Route, useLocation } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
 
-import Navbar from './components/navbar'
-import Home from './pages/home'
-import Blog from './pages/blog'
-import About from './pages/about'
-import Work from './pages/work'
-import Post from './pages/post'
-import Error from './pages/error'
-import ProjectDescription from './pages/project-description'
-
 import ThemeProvider from './context/theme-context'
+import Loader from './components/loader'
+
+const Navbar = lazy(() => import('./components/navbar'))
+const Home = lazy(() => import('./pages/home'))
+const Blog = lazy(() => import('./pages/blog'))
+const About = lazy(() => import('./pages/about'))
+const Work = lazy(() => import('./pages/work'))
+const Post = lazy(() => import('./pages/post'))
+const Error = lazy(() => import('./pages/error'))
+const ProjectDescription = lazy(() => import('./pages/project-description'))
 
 function App() {
   const location = useLocation()
 
   return (
-    <ThemeProvider>
-      <main className='font-inter dark:bg-dark text-dark dark:text-light min-h-screen bg-gray-200 pb-12'>
-        <Navbar path={location} />
-        <AnimatePresence exitBeforeEnter>
-          <Routes location={location} key={location.pathname}>
-            <Route path='/' element={<Home />} />
-            <Route path='/blog' element={<Blog />} />
-            <Route path='/blog/:slug' element={<Post />} />
-            <Route path='/work' element={<Work />} />
-            <Route path='/work/:project' element={<ProjectDescription />} />
-            <Route path='/about' element={<About />} />
-            <Route path='*' element={<Error />} />
-          </Routes>
-        </AnimatePresence>
-      </main>
-    </ThemeProvider>
+    <Suspense fallback={<Loader />}>
+      <ThemeProvider>
+        <main className='font-inter dark:bg-dark text-dark dark:text-light min-h-screen bg-gray-200 pb-12'>
+          <Navbar path={location} />
+          <AnimatePresence exitBeforeEnter>
+            <Routes location={location} key={location.pathname}>
+              <Route path='/' element={<Home />} />
+              <Route path='/blog' element={<Blog />} />
+              <Route path='/blog/:slug' element={<Post />} />
+              <Route path='/work' element={<Work />} />
+              <Route path='/work/:project' element={<ProjectDescription />} />
+              <Route path='/about' element={<About />} />
+              <Route path='*' element={<Error />} />
+            </Routes>
+          </AnimatePresence>
+        </main>
+      </ThemeProvider>
+    </Suspense>
   )
 }
 
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import BlockContent from '@sanity/block-content-to-react'
 import useSanity from '../hooks/useSanity'
-import TechStack from './tech-stack'
 import { client } from '../utils/client'
 import Container from '../components/container'
 import Loader from '../components/loader'
 import serializer from '../utils/serializer'
+
+const TechStack = lazy(() => import('./tech-stack'))
 
 const About = () => {
   const query = '*[_type == "author"][0]{bio}'
@@ -14,19 +15,21 @@ const About = () => {
   if (!userBio) return <Loader />
 
   return (
-    <Container title='About me'>
-      <section className='text-md mt-4 mb-10 rounded-2xl px-8 text-gray-600 dark:text-gray-400 md:px-12 lg:px-16'>
-        <article className='prose dark:text-light dark:prose-a:text-accent prose-a:text-darkAccent max-w-none leading-7 text-gray-600'>
-          <BlockContent
-            blocks={userBio.bio}
-            projectId={client.projectId}
-            dataset={client.dataset}
-            serializers={serializer}
-          />
-        </article>
-      </section>
-      <TechStack />
-    </Container>
+    <Suspense fallback={<Loader />}>
+      <Container title='About me'>
+        <section className='text-md mt-4 mb-10 rounded-2xl px-8 text-gray-600 dark:text-gray-400 md:px-12 lg:px-16'>
+          <article className='prose dark:text-light dark:prose-a:text-accent prose-a:text-darkAccent max-w-none leading-7 text-gray-600'>
+            <BlockContent
+              blocks={userBio.bio}
+              projectId={client.projectId}
+              dataset={client.dataset}
+              serializers={serializer}
+            />
+          </article>
+        </section>
+        <TechStack />
+      </Container>
+    </Suspense>
   )
 }
 
