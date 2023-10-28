6cdff2d22a91aedffb09e1457e94c2cd8d81b23e
Karthik <karunayak63@gmail.com>
2023-10-28 22:48:29 +0530

    Add react-helmet to the hook.

--- src/hooks/useTitle.js
+++ src/hooks/useTitle.js
@@ -1,11 +1,10 @@
-import { useEffect } from 'react'
+import { Helmet } from 'react-helmet';
 
-const useTitle = (title) => {
-  useEffect(() => {
-    document.title = title
-  }, [title])
+const useTitle = (title, description = '') => {
+  <Helmet>
+    <title>{title}</title>
+    <meta name='description' content={description} />
+  </Helmet>;
+};
 
-  return title
-}
-
-export default useTitle
+export default useTitle;
